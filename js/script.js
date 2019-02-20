c3cd90512692c4d043384f6bd97718bfe4749112
trankimtu <33793431+trankimtu@users.noreply.github.com>
2019-02-20 02:10:55 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-/*Empty js file*/
+
+let time = {
+  date: "date",
+  hour: 0,
+  minute: 0,
+  second: 0
+};
+
+function getTime() {
+  let today = new Date();
+  time.date = today.toDateString();  //"Tue Feb 19 2019"
+  time.hour = today.getHours();
+  time.minute = today.getMinutes();
+  time.second = today.getSeconds();
+}
+
+let message = {
+  message_time: time,
+  message_content: "message_content"
+};
+function search() {
+  console.log("Search output");
+}
+
+let history = "";
+function submitText() {
+  console.log("Testing");
+  getTime();
+  // document.getElementsByClassName("input_text").value = "Text Here";
+  // message.message_content = document.getElementsByClassName("input_text").value;
+  message.message_content = document.getElementById("input_text").value;
+  let outputMessage = time.date +" " + time.hour +":" + time.minute+ ":" + time.second + "\n" + message.message_content;
+  history += outputMessage + "\n\n";
+  document.getElementById("output_text").value = history;
+
+  document.getElementById("input_text").value = "";
+
+}

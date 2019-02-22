cad85f2da886cf90dda36657e9886cdd1f5e5949
trankimtu <33793431+trankimtu@users.noreply.github.com>
2019-02-21 23:20:01 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,6 +23,8 @@ function search() {
 }
 
 let history = "";
+document.getElementById("output_text").value = "";
+
 function submitText() {
   console.log("Testing");
   getTime();
@@ -36,3 +38,30 @@ function submitText() {
   document.getElementById("input_text").value = "";
 
 }
+
+function clearHistory() {
+  document.getElementById("output_text").value = "";
+  history = "";
+}
+
+
+// Image handles
+<script>
+   function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
+  previewFile();  //calls the function named previewFile()
+  </script>

3d5b370fcc8a52b6576762d109b78b5143b536b7
Alex Tran <37480707+quyen137@users.noreply.github.com>
2019-02-25 15:54:52 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,69 @@
-/*Empty js file*/
+
+let time = {
+  date: "date",
+  hour: 0,
+  minute: 0,
+  second: 0
+};
+
+function getTime() {
+  let today = new Date();
+  time.date = today.toDateString();  //"Tue Feb 19 2019"
+  time.hour = today.getHours();
+  time.minute = today.getMinutes();
+  time.second = today.getSeconds();
+}
+
+let message = {
+  message_time: time,
+  message_content: "message_content"
+};
+function search() {
+  console.log("Search output");
+}
+
+let history = "";
+document.getElementById("output_text").value = "";
+
+function submitText() {
+  console.log("Testing");
+  getTime();
+  // document.getElementsByClassName("input_text").value = "Text Here";
+  // message.message_content = document.getElementsByClassName("input_text").value;
+  message.message_content = document.getElementById("input_text").value;
+  let outputMessage = time.date +" " + time.hour +":" + time.minute+ ":" + time.second + "\n" + message.message_content;
+  history += outputMessage + "\n\n";
+  document.getElementById("output_text").value = history;
+
+  document.getElementById("input_text").value = "";
+
+}
+
+function clearHistory() {
+  document.getElementById("output_text").value = "";
+  history = "";
+}
+
+// start
+function newArticle(){
+  
+}
+
+// Image handles
+   function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
+  previewFile();  //calls the function named previewFile()
